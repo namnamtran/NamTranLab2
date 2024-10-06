389792698ecb079619ed31063a22b61e0e76c207
namnamtran <122743898+namnamtran@users.noreply.github.com>
2024-10-06 16:51:00 -0400

    fixed spelling issue

--- NamTranLab1/Controllers/EmployersController.cs
+++ NamTranLab1/Controllers/EmployersController.cs
@@ -1,4 +1,4 @@
-﻿// I, Nguyen Quov Nam Tran, student number 000876813, certify that this material is my
+﻿// I, Nguyen Quoc Nam Tran, student number 000876813, certify that this material is my
 // original work. No other person's work has been used without due
 // acknowledgement and I have not made my work available to anyone else.
 using System;
